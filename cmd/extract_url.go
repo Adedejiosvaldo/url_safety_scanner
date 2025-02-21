a1622958346659a9cbdbdb695ef587956edbc831
adedejiosvaldo <adedejiosvaldo@gmail.com>
2025-02-21 02:26:13 +0100

    Feat : URL extractor

---
+++ cmd/extract_url.go
@@ -0,0 +1,11 @@
+package cmd
+
+import "regexp"
+
+// Regex pattern for URL extraction
+var urlRegex = regexp.MustCompile(`https?://[^\s]+`)
+
+// Extract URLs from text
+func ExtractURLs(text string) []string {
+	return urlRegex.FindAllString(text, -1)
+}
